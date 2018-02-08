9d4a15f8299f9ffd94cb4581d820f86a403ea65f
luosam <luosam@gs.com>
2018-02-08 11:11:50 +0800

    split out

--- js/src/components/app.jsx
+++ js/src/components/app.jsx
@@ -3,75 +3,33 @@
  **/
 import React from 'react'
 
-/* select
+import {MySelect, MyOption} from './select.jsx'
 
-// old style createReactClass
-
-import createReactClass from 'create-react-class'
-
-var MySelect = createReactClass({
-
-  getInitialState: function(){
-    return {selected: false} 	 //this.state.selected = false;
-  },
-
-	select: function(event){
-		if(event.target.textContent === this.state.selected){//remove selection
-			this.setState({selected: false})
-			//update state
-		}else{
-		//add selection
-			this.setState({selected: event.target.textContent.trim()}) //update state
-		}
-	},
-
-  render: function () {
-    var mySelectStyle = {
-      border: '1px solid #999',
-      display: 'inline-block',
-      padding: '5px'
-    }
-
-    return <div style={mySelectStyle}>
-      <MyOption state={this.state.selected} select={this.select} value='Volvo' />
-      <MyOption state={this.state.selected} select={this.select} value='Saab' />
-      <MyOption state={this.state.selected} select={this.select} value='Mercedes' />
-      <MyOption state={this.state.selected} select={this.select} value='Audi' />
-    </div> // react div element, via JSX
-  }
-})
-
-// option
-var MyOption = createReactClass({
-
- render: function () {
-
-    var selectedStyle = { backgroundColor: 'red', color: '#fff', cursor: 'pointer' };
-    var unSelectedStyle = { cursor: 'pointer' };
-
-    console.log(`value: ${this.props.value}  ${this.props.state} ${this.props.value == this.props.state} `)
-
-    if(this.props.state == this.props.value){
-      return <div style={selectedStyle} onClick={this.props.select}> {this.props.value}</div> // react div element, via JSX
-    }else{
-      return <div style={unSelectedStyle} onClick={this.props.select}> {this.props.value}</div> // react div element, via JSX
-    }
-  }
-})
-*/
-
+/*
 class MySelect extends React.Component {
-  getInitialState () {
-    return {selected: false} 	 // this.state.selected = false;
+
+  constructor (props) {
+    super(props)
+    this.state = {selected : false}
+
+    // bind this to event
+    this.select = this.select.bind(this);
   }
 
   select (event) {
+
+    console.log(`target: ${event.target.textContext}  ${this.state.selected} `)
+
     if (event.target.textContent === this.state.selected) { // remove selection
+      //this.state.selected = false
       this.setState({selected: false})
       // update state
     } else {
       // add selection
-      this.setState({selected: event.target.textContent.trim()}) // update state
+      // this.setState({selected: event.target.textContent.trim()}) // update state
+      if (event.target.textContext){
+        this.setState( {selected : event.target.textContext})
+      }
     }
   }
 
@@ -91,8 +49,12 @@ class MySelect extends React.Component {
   }
 }
 
-/* option */
 class MyOption extends React.Component {
+
+  constructor (props){
+    super(props)
+  }
+
   render () {
     var selectedStyle = { backgroundColor: 'red', color: '#fff', cursor: 'pointer' }
     var unSelectedStyle = { cursor: 'pointer' }
@@ -107,6 +69,7 @@ class MyOption extends React.Component {
   }
 }
 
+*/
 export default class App extends React.Component {
   render () {
     return (
---
+++ js/src/components/select.jsx
@@ -0,0 +1,58 @@
+/*
+ *
+ */
+import React from 'react'
+import createReactClass from 'create-react-class'
+
+var MySelect = createReactClass({
+
+  getInitialState: function(){
+    return {selected: false} 	 //this.state.selected = false;
+  },
+
+	select: function(event){
+		if(event.target.textContent === this.state.selected){//remove selection
+			this.setState({selected: false})
+			//update state
+		}else{
+		//add selection
+			this.setState({selected: event.target.textContent.trim()}) //update state
+		}
+	},
+
+  render: function () {
+    var mySelectStyle = {
+      border: '1px solid #999',
+      display: 'inline-block',
+      padding: '5px'
+    }
+
+    return <div style={mySelectStyle}>
+      <MyOption state={this.state.selected} select={this.select} value='Volvo' />
+      <MyOption state={this.state.selected} select={this.select} value='Saab' />
+      <MyOption state={this.state.selected} select={this.select} value='Mercedes' />
+      <MyOption state={this.state.selected} select={this.select} value='Audi' />
+    </div> // react div element, via JSX
+  }
+})
+
+// option
+var MyOption = createReactClass({
+
+ render: function () {
+
+    var selectedStyle = { backgroundColor: 'red', color: '#fff', cursor: 'pointer' };
+    var unSelectedStyle = { cursor: 'pointer' };
+
+    console.log(`value: ${this.props.value}  ${this.props.state} ${this.props.value == this.props.state} `)
+
+    if(this.props.state == this.props.value){
+      return <div style={selectedStyle} onClick={this.props.select}> {this.props.value}</div> // react div element, via JSX
+    }else{
+      return <div style={unSelectedStyle} onClick={this.props.select}> {this.props.value}</div> // react div element, via JSX
+    }
+  }
+})
+
+
+export {MySelect, MyOption}
